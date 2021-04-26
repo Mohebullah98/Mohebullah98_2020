cf1a941b53f778c17e4d0b3946574bb5659b1574
Mohebullah Mir <>
2021-04-26 23:32:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void convert(int number){
+    String s = Integer.toString(number);
+    for(int i=0;i<s.length;i++){
+
+    }
+  }
+  public static void main(String[] args) {
+    int firstNumber = Integer.parseInt(args[0]);
+    int secondNumber = Integer.parseInt(args[1]);
+    int thirdNumber = Integer.parseInt(args[2]);
+
+    
+  }
+}
