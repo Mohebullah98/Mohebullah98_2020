0f036d141e310f48b31a8b03415aff9b4a7a48f6
Mohebullah Mir <>
2021-04-27 20:24:56 +0000

    Fixed comments

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 class Main {
   public static void convert(int number){
-    int digit;// The digit we will print
+    int digit;// The digit we need to read
     char digitChar;// need to convert that digit to char in order to seperate digits
     String digitWord="";//The word we will print
     String s = Integer.toString(number);//Convert number to string so we can seperate and iterate through each digit.
-    for(int i=0;i<s.length();i++){
+    for(int i=0;i<s.length();i++){// iterate through each digit in the number to get their phonetic value
       digitChar=s.charAt(i);
       digit = Character.getNumericValue(digitChar);// get the int value back from each individual digit
       switch(digit){//switch statement to map digits to their words.
